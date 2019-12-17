00ed0c59ddc10f09907a2829b8d2a2e377b43fb3
christiani_kunda <c.iradukunda@irembo.com>
2019-12-17 13:47:02 +0200

    added the hello controller to test that service is getting through the gateway

---
+++ src/test/java/com/christian/service/ReportServerTests.java
@@ -0,0 +1,16 @@
+package com.christian.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportServerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
